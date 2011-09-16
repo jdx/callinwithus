bc90f3a39efee41f2b00c91fc525dbdd07cc2c20
Jeff Dickey <dickeytk@gmail.com>
2011-09-15 19:52:11 -0700

    Added conferencing

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,16 +1,28 @@
 class TwilioController < ApplicationController
   def call
-    if params['Digits']
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Welcome to kawlin with us. Please enter your pin number.', :voice => 'woman'
+      r.Gather 'numDigits' => 6, :action => twilio_call_with_code_url
+    end
+
+    return render :text => response.text
+  end
+
+
+  def call_with_code
+    conference_call = ConferenceCall.find_by_code(params['Digits'])
+    if conference_call
       response = Twilio::TwiML::Response.new do |r|
-        r.Say "You entered #{ params['Digits'] }"
+        r.Say 'Entering conference room.', :voice => 'woman'
+        r.Conference conference_call.code
       end
     else
       response = Twilio::TwiML::Response.new do |r|
-        r.Say 'Welcome to kawlin withus, please enter your pin number.', :voice => 'woman'
-        r.Gather 'numDigits' => 6
+        r.Say 'Invalid pin. Please try again.', :voice => 'woman'
+        r.Redirect twilio_call_url
       end
     end
 
-    render :text => response.text
+    return render :text => response.text
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Callinwithus::Application.routes.draw do
   resources :conference_calls, :only => [ :new, :create, :show ]
-  match 'twilio/call' => 'twilio#call'
-  #post 'twilio/call' => 'twilio#call'
+  post 'twilio/call' => 'twilio#call'
+  post 'twilio/call_with_code' => 'twilio#call_with_code'
   root :to => 'conference_calls#new'
 end
