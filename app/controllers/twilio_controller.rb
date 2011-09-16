6ef0c85b237f44acb837e7f77ae32030599eb10d
Jeff Dickey <dickeytk@gmail.com>
2011-09-15 19:44:46 -0700

    twilio tweaks

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,13 +1,13 @@
 class TwilioController < ApplicationController
   def call
-    if params[:digits]
+    if params['Digits']
       response = Twilio::TwiML::Response.new do |r|
-        r.Say "You entered #{ params[:digits] }"
+        r.Say "You entered #{ params['Digits'] }"
       end
     else
       response = Twilio::TwiML::Response.new do |r|
-        r.Say 'Welcome to Call In With Us, please enter your pin number.', :voice => 'woman'
-        r.Gather :num_digits => 6
+        r.Say 'Welcome to kawlin withus, please enter your pin number.', :voice => 'woman'
+        r.Gather 'numDigits' => 6
       end
     end
 
