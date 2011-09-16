a3b116318f3d3d61ffcb6ab3c9f3b03eba4bcbe3
Jeff Dickey <dickeytk@gmail.com>
2011-09-15 17:48:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callinwithus::Application.config.secret_token = '85fb9a24114bafe5c68c699004221ca926d17c2d148f8b995aae17d2d5deaae236023069f832d15a328899135c923e7c693b4e47d2a51dac4fd44a04755abfa5'
