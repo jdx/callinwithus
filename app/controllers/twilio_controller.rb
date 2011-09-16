ee913850eb896617ba5c092fed15b4de6152f214
Jeff Dickey <dickeytk@gmail.com>
2011-09-15 19:40:22 -0700

    Updated twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,14 @@
 class TwilioController < ApplicationController
   def call
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'hello there', :voice => 'woman'
+    if params[:digits]
+      response = Twilio::TwiML::Response.new do |r|
+        r.Say "You entered #{ params[:digits] }"
+      end
+    else
+      response = Twilio::TwiML::Response.new do |r|
+        r.Say 'Welcome to Call In With Us, please enter your pin number.', :voice => 'woman'
+        r.Gather :num_digits => 6
+      end
     end
 
     render :text => response.text
