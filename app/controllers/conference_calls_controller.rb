8b881585254740f52f18dbdab4592d6ff45fef0d
dickeyxxx <jeff@dickey.xxx>
2012-10-30 16:34:46 -0700

    added deletion

--- app/controllers/conference_calls_controller.rb
+++ app/controllers/conference_calls_controller.rb
@@ -1,6 +1,8 @@
 class ConferenceCallsController < ApplicationController
 
   def new
+    @old_thing = ConferenceCall.order('created_at').limit(1).first
+    @old_thing.destroy if @old_thing
     @conference_call = ConferenceCall.create!
     redirect_to "/#{ @conference_call.code_for_display }"
   end
